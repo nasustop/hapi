5b13035c1220a6e417eb6718524b2b0fb776de4f
eaglexpf <1121252990@qq.com>
2022-11-19 17:05:11 +0800

    add watcher config

---
+++ config/autoload/watcher.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of HapiBase.
+ *
+ * @link     https://www.nasus.top
+ * @document https://wiki.nasus.top
+ * @contact  xupengfei@xupengfei.net
+ * @license  https://github.com/nasustop/hapi/blob/master/LICENSE
+ */
+use Hyperf\Watcher\Driver\ScanFileDriver;
+
+return [
+    'driver' => ScanFileDriver::class,
+    'bin' => 'php',
+    'watch' => [
+        'dir' => ['app', 'config', 'routes', 'src'],
+        'file' => ['.env'],
+        'scan_interval' => 2000,
+    ],
+];
