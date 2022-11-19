253b5797be224574c744da405b52d96835d51803
eaglexpf <1121252990@qq.com>
2022-11-19 17:04:50 +0800

    add app repository

---
+++ app/Repository/Repository.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of HapiBase.
+ *
+ * @link     https://www.nasus.top
+ * @document https://wiki.nasus.top
+ * @contact  xupengfei@xupengfei.net
+ * @license  https://github.com/nasustop/hapi/blob/master/LICENSE
+ */
+namespace App\Repository;
+
+class Repository extends \Nasustop\HapiBase\Repository\Repository
+{
+}
