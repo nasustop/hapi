2a09e629ca6f45c5661df3f9daa14edcf2e694d9
eaglexpf <1121252990@qq.com>
2022-12-01 21:46:53 +0800

    update cors middleware

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -25,7 +25,9 @@ class CorsMiddleware implements MiddlewareInterface
         $response = $response->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             // Headers 可以根据实际情况进行改写。
-            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,X-Token');
+            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+
+        Context::set(ResponseInterface::class, $response);
 
         if ($request->getMethod() == 'OPTIONS') {
             return $response;
